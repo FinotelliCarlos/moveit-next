61b1895c2f9bd4db48f64a845ba79591f9d15f96
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-02-24 17:32:50 -0300

    styled challengebox active and notactive

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,34 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+    const hasActiveChallenge = true;
+    
     return (
         <div className={styles.challengeBoxCountainer}>
-            <div className={styles.challengeNotActive}>
-                <strong>Finalize um ciclo para receber um desafio</strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level Up"/>
-                    Suba de Level completando desafios.
-                </p>
-            </div>
+            {hasActiveChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400xp</header>
+                    <main>
+                        <img src="icons/body.svg" />
+                        <strong>Novo desafio</strong>
+                        <p>Levante e faça 10 agachamentos a cada 1 minuto por 5 minutos.</p>
+                    </main>
+
+                    <footer>
+                        <button type="button" className={styles.challengeFailedbutton}>Falhei</button>
+                        <button type="button" className={styles.challengeSucceededbutton}>Completei</button>
+                    </footer>
+                </div>
+            ) : (
+                <div className={styles.challengeNotActive}>
+                    <strong>Finalize um ciclo para receber um desafio</strong>
+                    <p>
+                        <img src="icons/level-up.svg" alt="Level Up"/>
+                        Suba de Level completando desafios.
+                    </p>
+                </div>
+            )}
+
         </div>
     )
 }
