0a73987e14cf3da9d1d85ecce14df34e953a2902
Finotelli, Carlos Eduardo Pereira <carlos.finotelli@outlook.com>
2021-02-25 10:17:24 -0300

    config countdown contexts

--- src/Contexts/CountdownContext.tsx
+++ src/Contexts/CountdownContext.tsx
@@ -1,18 +1,64 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { ChallengesContext } from "./ChallengesContext";
 
 interface CountdownContextData {
-
+    minutes: number;
+    secunds: number;
+    hasFinished: boolean;
+    isActive: boolean;
+    startCountdown: () => void;
+    resetCountdown: () => void;
 }
 
 interface CountdownProviderProps {
     children: ReactNode;
 }
 
-const CountdownContext = createContext({} as CountdownContextData);
+export const CountdownContext = createContext({} as CountdownContextData);
+
+let countdownTimeout: NodeJS.Timeout;
 
 export function CountdownProvider({children}: CountdownProviderProps){
+    const { startNewChallenge } = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(0.1 * 60); //define quantidade de minutos
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
+
+    const minutes = Math.floor(time / 60); //arredonda o valor para baixo, calcula o tempo
+    const secunds = time % 60; //resulta no resto da divisão exibindo os segundos
+
+    function startCountdown(){
+        setIsActive(true);
+    }
+
+    function resetCountdown(){
+        clearTimeout(countdownTimeout);
+        setIsActive(false);
+        setTime(0.1 * 60);
+    }
+
+    useEffect(() => {
+        if(isActive && time > 0){
+            countdownTimeout = setTimeout(() => {
+                setTime(time - 1);
+            }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
+        }
+    }, [isActive, time])
+
     return (
-        <CountdownContext.Provider value={{}}>
+        <CountdownContext.Provider value={{
+            minutes,
+            secunds,
+            hasFinished,
+            isActive,
+            startCountdown,
+            resetCountdown,
+        }}>
             {children}
         </CountdownContext.Provider>
     )
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,45 +1,14 @@
-import { useState, useEffect, useContext } from 'react'
-import { ChallengesContext } from '../Contexts/ChallengesContext';
+import { useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css'
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-    const { startNewChallenge } = useContext(ChallengesContext);
-
-    const [time, setTime] = useState(0.1 * 60); //define quantidade de minutos
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    const minutes = Math.floor(time / 60); //arredonda o valor para baixo, calcula o tempo
-    const secunds = time % 60; //resulta no resto da divisão exibindo os segundos
+    const { minutes, secunds, hasFinished, isActive, startCountdown, resetCountdown } = useContext(CountdownContext);
 
     const [minuteLeft, minuteRight] = String(minutes).padStart(2,'0').split('');
     //torna os minutos em string,se não houver 2 casas inserir zero,separa os minutos em array (esquerdo e direito)
     const [secundLeft, secundRight] = String(secunds).padStart(2,'0').split('');
     //torna os segundos em string,se não houver 2 casas inserir zero,separa os segundos em array (esquerdo e direito)
-
-    function StartCountdown(){
-        setIsActive(true);
-    }
-
-    function resetCountdown(){
-        clearTimeout(countdownTimeout);
-        setIsActive(false);
-        setTime(0.1 * 60);
-    }
-
-    useEffect(() => {
-        if(isActive && time > 0){
-            countdownTimeout = setTimeout(() => {
-                setTime(time - 1);
-            }, 1000)
-        } else if (isActive && time === 0) {
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive, time])
 
     return (
         <div>
@@ -64,7 +33,7 @@ export function Countdown() {
                         Abandonar Ciclo
                     </button>
                 ) : (
-                    <button type="button" className={styles.countdownButton} onClick={StartCountdown}>
+                    <button type="button" className={styles.countdownButton} onClick={startCountdown}>
                         Iniciar um ciclo
                     </button>
                 )}
